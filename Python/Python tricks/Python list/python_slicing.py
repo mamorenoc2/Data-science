27f44627d9e9414f89cb0ecd14e26b506905ea14
Manuela Moreno <49201450+mamorenoc19@users.noreply.github.com>
2021-07-05 07:27:24 -0500

    Create python_slicing.py

---
+++ Python/Python tricks/Python list/python_slicing.py
@@ -0,0 +1,6 @@
+numeros = [1,2,3,4,5,6,7,8,9,10]
+
+tres_primeros                = numeros[:3]
+despues_de_los_tres_primeros = numeros[3:]
+tres_ultimos                 = numeros[-3:]
+antes_de_los_tres_ultimos    = numeros[:-3]
