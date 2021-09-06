3492180495d68159b9b2ddd8a8fa19818609239c
Manuela Moreno <49201450+mamorenoc19@users.noreply.github.com>
2021-09-06 06:20:01 -0500

    Update dictionary.py
    ADD / APPEND VALUES TO AN EXISTING KEY in python

--- Python/Python tricks/Python Dictionaries/dictionary.py
+++ Python/Python tricks/Python Dictionaries/dictionary.py
@@ -144,3 +144,20 @@ pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58
 #for value, key in pct_women_in_occupation.items():
 #  print('Women make up '+str(key)+' percent of '+str(value)+'s')
 
+
+#---------------------------- ADD / APPEND VALUES TO AN EXISTING KEY --------------------------------#
+
+def append_value(dict_obj, key, value):
+    # Check if key exist in dict or not
+    if key in dict_obj:
+        # Key exist in dict.
+        # Check if type of value of key is list or not
+        if not isinstance(dict_obj[key], list):
+            # If type is not list then make it list
+            dict_obj[key] = [dict_obj[key]]
+        # Append the value in list
+        dict_obj[key].append(value)
+    else:
+        # As key is not in dict,
+        # so, add key-value pair
+        dict_obj[key] = value
