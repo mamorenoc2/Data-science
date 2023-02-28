1330ffe31db274959917910181fc7721f0fbbad9
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-02-28 18:38:23 -0500

    Looking at BMI Factor
    See some individual factors to see what role each one plays in our estimation, in this case the BMI

--- Python/Python proyects/Pytho dictionaries/Medical Insurance Project/basic.py
+++ Python/Python proyects/Pytho dictionaries/Medical Insurance Project/basic.py
@@ -19,7 +19,11 @@ change_in_insurance_cost  = new_insurance_cost - insurance_cost
 print(''' The change in cost of insurance after increasing the age by 4 years ''' + str(change_in_insurance_cost)+ ' dollars')
 
 # BMI Factor
-
+age -= 4
+bmi += 3.1
+new_insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+change_in_insurance_cost  = new_insurance_cost - insurance_cost
+print(''' The change in estimated insurance cost after increasing BMI by 3.1 is ''' + str(change_in_insurance_cost)+ ' dollars')
 
 # Male vs. Female Factor
 
