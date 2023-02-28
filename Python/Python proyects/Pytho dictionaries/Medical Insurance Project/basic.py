2b705b9ee7c4c59adba6837a39e758a141bd7008
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-02-28 18:33:38 -0500

    Looking at Age Factor
    See some individual factors to see what role each one plays in our estimation, in this case the age

--- Python/Python proyects/Pytho dictionaries/Medical Insurance Project/basic.py
+++ Python/Python proyects/Pytho dictionaries/Medical Insurance Project/basic.py
@@ -1,6 +1,5 @@
-# Using a formula that estimates a person’s yearly insurance costs, we will investigate how different factors such as age, sex, BMI, etc. affect the prediction.
 # create the initial variables below
-age = 28
+age = 28 
 sex = 0
 bmi = 26.2
 num_of_children = 3
@@ -13,7 +12,11 @@ insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 2400
 print(''' This person's inurance cost is ''' + str(insurance_cost)+ ' dollars')
 
 # Age Factor
+age += 4
+new_insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+change_in_insurance_cost  = new_insurance_cost - insurance_cost
 
+print(''' The change in cost of insurance after increasing the age by 4 years ''' + str(change_in_insurance_cost)+ ' dollars')
 
 # BMI Factor
 
