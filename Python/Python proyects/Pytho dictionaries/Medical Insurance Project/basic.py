e0c33184db6938521e07f05a9084b88e98dde0c2
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-02-28 18:46:40 -0500

    Smoker Factor
    See some individual factors to see what role each one plays in our estimation, in this case the smoker factor

--- Python/Python proyects/Pytho dictionaries/Medical Insurance Project/basic.py
+++ Python/Python proyects/Pytho dictionaries/Medical Insurance Project/basic.py
@@ -9,28 +9,34 @@ smoker = 0
 
 insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 
-print(''' This person's inurance cost is ''' + str(insurance_cost)+ ' dollars')
+print('''This person's inurance cost is ''' + str(insurance_cost)+ ' dollars')
 
 # Age Factor
 age += 4
 new_insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 change_in_insurance_cost  = new_insurance_cost - insurance_cost
 
-print(''' The change in cost of insurance after increasing the age by 4 years ''' + str(change_in_insurance_cost)+ ' dollars')
+print('''The change in cost of insurance after increasing the age by 4 years ''' + str(change_in_insurance_cost)+ ' dollars')
 
 # BMI Factor
 age -= 4
 bmi += 3.1
 new_insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 change_in_insurance_cost  = new_insurance_cost - insurance_cost
-print(''' The change in estimated insurance cost after increasing BMI by 3.1 is ''' + str(change_in_insurance_cost)+ ' dollars')
+print('''The change in estimated insurance cost after increasing BMI by 3.1 is ''' + str(change_in_insurance_cost)+ ' dollars')
 
 # Male vs. Female Factor
 bmi -= 3.1
 sex = 1
 new_insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 change_in_insurance_cost  = new_insurance_cost - insurance_cost
-print(''' The change in estimated cost for being male instead of female is ''' + str(change_in_insurance_cost)+ ' dollars')
+print('''The change in estimated cost for being male instead of female is ''' + str(change_in_insurance_cost)+ ' dollars')
 
 
-# Extra Practice
+# Smocker Factor
+sex = 0
+smoker = 1
+new_insurance_cost = 250 * age -128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+print('''The change in estimated cost for being a smoker is  ''' + str(change_in_insurance_cost) + ' dollars')
+
