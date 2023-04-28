a73ad23c2a11c03822cb60cf6ae2ecd783270be9
mamorenoc2 <49201450+mamorenoc2@users.noreply.github.com>
2023-04-28 16:11:37 +0000

    Changes in dict comprehesion

--- Education/Platzi/Fundamentos_de_programacion/Comprehensions_Funciones_y_Manejo_de_Errores/05_dict.py
+++ Education/Platzi/Fundamentos_de_programacion/Comprehensions_Funciones_y_Manejo_de_Errores/05_dict.py
@@ -1,3 +1,4 @@
+import random
 #diccionarios de numeros pares
 dict = {}
 for i in range(1,10):
@@ -5,7 +6,22 @@ for i in range(1,10):
 
 #list comprehesions in dictionaries
 dict_comprehesion = {i: i * 2 for i in range(1, 10) }
-print(dict)
-print(dict_comprehesion)
+#print(dict)
+#print(dict_comprehesion)
 
+countries = ['col', 'mex', 'bol', 'pe']
+population = {}
 
+for country in countries:
+  population[country] = random.randint(1,100)
+
+#print(population)
+
+population_v1 = {country: random.randint(1,100) for country in countries}
+#print(population_v1)
+
+names = ['nico', 'zule', 'santi']
+ages = [12, 56, 98]
+
+new_dict = {name:age for (name, age) in zip(names, ages)}
+print(new_dict)
