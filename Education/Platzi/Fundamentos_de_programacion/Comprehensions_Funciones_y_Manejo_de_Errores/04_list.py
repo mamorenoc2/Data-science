8f7de453bddd24b74eee3c8f55a15d190ad9a36b
mamorenoc2 <49201450+mamorenoc2@users.noreply.github.com>
2023-04-28 14:56:41 +0000

    Modification in list comprehesions

--- Education/Platzi/Fundamentos_de_programacion/Comprehensions_Funciones_y_Manejo_de_Errores/04_list.py
+++ Education/Platzi/Fundamentos_de_programacion/Comprehensions_Funciones_y_Manejo_de_Errores/04_list.py
@@ -4,3 +4,7 @@ print(numbers)
 
 numbers_by_two = [i*2 for i in range(1,11)]
 print(numbers_by_two)
+
+#Programa, multiplicar un numero si este es par con list comprehesions de 1 a 20
+numbers_2 = [i*2 for i in range(1, 20) if i%2 == 0]
+print(numbers_2)
