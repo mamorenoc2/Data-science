fa72a5f1854448a6a9e09f6bad27f1f71ad60b18
mamorenoc2 <49201450+mamorenoc2@users.noreply.github.com>
2023-05-02 17:39:35 +0000

    More Data Structure with python

---
+++ Education/Platzi/Fundamentos_de_programacion/Comprehensions_Funciones_y_Manejo_de_Errores/07_func.py
@@ -0,0 +1,9 @@
+def my_print(text):
+  print(text)
+
+my_print('hola')
+
+def suma(a, b):
+  print(a+b)
+
+suma(10,11)
---
+++ Education/Platzi/Fundamentos_de_programacion/Comprehensions_Funciones_y_Manejo_de_Errores/08_return.py
@@ -0,0 +1,8 @@
+def sum_with_range(min_, max_):
+  suma = 0
+  for x in range(min_,max_):
+    suma += x
+  return suma
+
+resultado = sum_with_range(1, 100)
+print(suma)
