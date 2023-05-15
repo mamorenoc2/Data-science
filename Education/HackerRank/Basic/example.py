36350812b8141241793d7935d114299de7aacd1d
mamorenoc2 <49201450+mamorenoc2@users.noreply.github.com>
2023-05-15 19:24:20 +0000

    jumm

--- Education/HackerRank/Basic/example.py
+++ Education/HackerRank/Basic/example.py
@@ -1,27 +1,7 @@
-# pasta = "tomato, basil, garlic, salt, pasta, olive oil"
-# apple_pie = "apple, sugar, salt, cinnamon, flour, egg, butter"
-# ratatouille = "aubergine, carrot, onion, tomato, garlic, olive oil, pepper, salt"
-# chocolate_cake = "chocolate, sugar, salt, flour, coffee, butter"
-# omelette = "egg, milk, bacon, tomato, salt, pepper"
+x = int(input())
+y = int(input())
 
-# ingredient = input()
+lista_suma = sum([x, y])
 
-# if ingredient in pasta:
-#     print('pasta time!')
-# if ingredient in apple_pie:
-#     print('apple pie time!')
-# if ingredient in ratatouille:
-#     print('ratatouille time!')
-# if ingredient in chocolate_cake:
-#     print('chocolate cake time!')
-# if ingredient in omelette:
-#     print('omelette time')
-  
-number = int(input())
-word = input()
-
-# write a condition for plurals
-if number > 1:
-    word += 's'
-
-print(number, word)
+print(lista_suma)
+help(round)
